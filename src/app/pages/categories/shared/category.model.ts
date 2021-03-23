9bbedfaa81d8a71eff533107f1e56044f09411d9
Gabriel Ribeiro <gabrielkaina99@gmail.com>
2021-03-23 13:24:04 -0300

    setting up EntriesModule

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,6 +1,7 @@
 export class Category {
-  constructor (public id?: number,
+  constructor (
+    public id?: number,
     public name?: string,
     public description?: string
-    ) {}
+  ) {}
 }
